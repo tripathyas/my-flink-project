56ca3c933063ed7b6571acbef2a2f8aed86e99e4
ashutosh tripathy <ashutosh_tripathy@outlook.com>
2022-11-07 13:32:43 +0000

    Add statefull tranformation example

---
+++ src/main/java/com/example/streaming/ReadingAndWritingFilesv2.java
@@ -0,0 +1,53 @@
+package com.example.streaming;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.io.FilePathFilter;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.api.java.io.TextInputFormat;
+import org.apache.flink.configuration.RestOptions;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
+import org.apache.flink.streaming.api.functions.source.FileProcessingMode;
+import org.apache.flink.core.fs.Path;
+
+
+public class ReadingAndWritingFilesv2 {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+        conf.setInteger(RestOptions.PORT, 8082);
+
+        final StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.createLocalEnvironment(1, conf);
+
+        String path = "src/main/resources/rioOlympics.txt";
+        String outputPath = "src/main/resources/streamingSink";
+
+        TextInputFormat format = new TextInputFormat(new Path(path));
+        format.setFilesFilter(FilePathFilter.createDefaultFilter());
+
+        DataStream<String> inputStream = env.readFile(format, path,
+                FileProcessingMode.PROCESS_CONTINUOUSLY, 100);
+
+        DataStream<String> headerStream = inputStream.filter(
+                (FilterFunction<String>) input -> input.startsWith("Rio"));
+
+        DataStream<String> filterData = inputStream.filter(
+                (FilterFunction<String>) input -> input.contains("Gold:0"));
+
+        DataStream<String> finalStream = headerStream.union(filterData);
+
+        final StreamingFileSink<String> sink = StreamingFileSink
+                .forRowFormat(new Path(outputPath),
+                              new SimpleStringEncoder<String>("UTF-8"))
+                .build();
+
+        finalStream.addSink(sink);
+        finalStream.print();
+
+        env.execute("Processing File Continuously");
+    }
+}
